6e6f58460161059a155c8c2b477fc42f6d56a933
front-ed <edwinschutjes@gmail.com>
2023-07-01 12:34:03 +0200

    add: relative background

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <main>
-      <Header />
-    </main>
+    <>
+      <div className="bg-top" aria-hidden="true"></div>
+      <main>
+        <Header />
+      </main>
+    </>
   );
 }
 
