bad9ea2b3eafe19dc8dd13b7083adc18a4d45179
front-ed <edwinschutjes@gmail.com>
2023-07-01 15:00:56 +0200

    chore: prep imgs for import

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,20 @@
+import BTC from "../assets/btc.png";
+import ETH from "../assets/ether.png";
+import USDT from "../assets/tether.png";
+import DOGE from "../assets/doge.png";
+
 const Cards = ({ exchange }) => {
   return (
-    <section className="main-cards">
+    <section className="main-cards-container">
       {exchange.map((coin) => {
         // Destructure
         const { id, symbol, name, priceUsd } = coin;
 
-        return <article key={id}>{name}</article>;
+        return (
+          <article className="main-card" key={id}>
+            {name}
+          </article>
+        );
       })}
     </section>
   );
