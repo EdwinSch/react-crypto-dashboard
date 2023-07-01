8e92003da526311a61527231baf95441b7d0618d
front-ed <edwinschutjes@gmail.com>
2023-07-01 12:16:05 +0200

    add: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="header-titles">
+        <h1>social media dashboard</h1>
+        <h2>total something</h2>
+      </div>
+      <div className="color-switch">
+        <label htmlFor="">dark mode</label>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
