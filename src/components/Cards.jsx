1e3fd6c5bc774cfd838371522e51d2cb51fa9a13
front-ed <edwinschutjes@gmail.com>
2023-07-02 13:59:43 +0200

    add: convert and map API data + some styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,10 +9,17 @@ const Cards = ({ exchange }) => {
       {exchange.map((coin) => {
         // Destructure
         const { id, symbol, name, priceUsd } = coin;
+        // Convert incoming string to float number
+        let conversion = parseFloat(priceUsd).toFixed(2);
 
         return (
           <article className="main-card" key={id}>
-            {name}
+            <div className="card-header">
+              <img src={BTC} alt={name} />
+              <h3>{name}</h3>
+            </div>
+            <p className="value">${conversion}</p>
+            <p className="percentage">12% Today</p>
           </article>
         );
       })}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header>
       <div className="header-titles">
-        <h1>social media dashboard</h1>
+        <h1>crypto dashboard</h1>
         <h2>total something</h2>
       </div>
       <div className="color-switch">
